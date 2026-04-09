99f9f1f866694bb5692c7beae855ec076d2620d9
Camila Parise <camilinhaparis@hotmail.com>
2026-04-10 01:14:22 +0200

    add change image

--- js/script.js
+++ js/script.js
@@ -2,13 +2,17 @@ const html = document.querySelector("html");
 const focoBt = document.querySelector(".app__card-button--foco");
 const curtoBt = document.querySelector(".app__card-button--curto");
 const longoBt = document.querySelector(".app__card-button--longo");
+const banner = document.querySelector(".app__image");
 
 focoBt.addEventListener("click", () => {
   html.setAttribute("data-contexto", "foco");
+  banner.setAttribute("src", "./assets/images/foco.png");
 });
 curtoBt.addEventListener("click", () => {
   html.setAttribute("data-contexto", "descanso-curto");
+  banner.setAttribute("src", "./assets/images/descanso-curto.png");
 });
 longoBt.addEventListener("click", () => {
   html.setAttribute("data-contexto", "descanso-longo");
+  banner.setAttribute("src", "./assets/images/descanso-longo.png");
 });
